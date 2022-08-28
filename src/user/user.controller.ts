8a5df3f222dd8b6db26ccaf0c0247f1f9e57ce26
krutulkamil <krutulkamil@onet.pl>
2022-08-28 21:30:37 +0200

    user controller methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Param, Patch, Put, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { Auth } from "../auth/decorators/auth.decorator";
 import { CurrentUser } from "./user.decorator";
+import { UserDto } from "./user.dto";
 
 @Controller("user")
 export class UserController {
@@ -11,5 +12,32 @@ export class UserController {
     @Auth()
     async getProfile(@CurrentUser('id') id: number) {
         return this.userService.byId(id);
+    };
+
+    @Get("by-id/:id")
+    @Auth()
+    async getUser(@Param('id') id: string) {
+        return this.userService.byId(+id);
+    };
+
+    @UsePipes(new ValidationPipe())
+    @HttpCode(200)
+    @Put(':id')
+    @Auth()
+    async updateUser(@Param('id') id: string, @Body() dto: UserDto) {
+        return this.userService.updateProfile(+id, dto);
+    };
+
+    @UsePipes(new ValidationPipe())
+    @HttpCode(200)
+    @Patch('subscriber/:channelId')
+    @Auth()
+    async subscribeToChannel(@CurrentUser('id') id: number, @Param('channelId') channelId: string) {
+        return this.userService.subscribe(+id, +channelId);
+    };
+
+    @Get()
+    async getUsers() {
+        return this.userService.getAll();
     };
 }
