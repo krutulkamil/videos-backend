23e591d9d6a14202e3d0c3159bd763006dc6461f
krutulkamil <krutulkamil@onet.pl>
2022-08-28 21:22:06 +0200

    user decorator + getProfile method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { CurrentUser } from "./user.decorator";
 
-@Controller('user')
+@Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {}
+
+    @Get("profile")
+    @Auth()
+    async getProfile(@CurrentUser('id') id: number) {
+        return this.userService.byId(id);
+    };
 }
