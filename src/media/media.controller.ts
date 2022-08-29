42de4cf08a5e60dadeaa6c03f3319758556cf64b
krutulkamil <krutulkamil@onet.pl>
2022-08-29 21:10:43 +0200

    ability to upload media file (video)

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { MediaService } from './media.service';
+import { Controller, HttpCode, Post, Query, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { MediaService } from "./media.service";
 
-@Controller('media')
+@Controller("media")
 export class MediaController {
-  constructor(private readonly mediaService: MediaService) {}
+    constructor(private readonly mediaService: MediaService) {}
+
+    @HttpCode(200)
+    @Post()
+    @Auth()
+    @UseInterceptors(FileInterceptor("media"))
+    async uploadMediaFile(@UploadedFile() mediaFile: Express.Multer.File, @Query("folder") folder?: string) {
+        return this.mediaService.saveMedia(mediaFile, folder);
+    };
 }
--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MediaService } from './media.service';
-import { MediaController } from './media.controller';
+import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { MediaService } from "./media.service";
+import { MediaController } from "./media.controller";
+import { path } from "app-root-path";
 
 @Module({
-  controllers: [MediaController],
-  providers: [MediaService]
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: `${path}/uploads`,
+            serveRoot: "/uploads"
+        })
+    ],
+    controllers: [MediaController],
+    providers: [MediaService]
 })
-export class MediaModule {}
+export class MediaModule {
+}
